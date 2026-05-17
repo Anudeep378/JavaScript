467035aa730ed921b1c9318cb4d0b2c007023dcf
Anudeep Singh <anudeepsinghh@gmail.com>
2026-05-17 14:37:32 +0000

    container config and variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+console.log("hello this is me")
+const accountId = "abcdef" // if we change this we get error accountId = 2 then error
+let accountEmail = "anudeep@gmail.com"
+var accountPassword = "123445"
+accountCity = "hyd"
+// variables can be declared in both let and var ,but var not used due to scope , {}
+
+accountEmail = "xyz@gmail"
+accountPassword = "2323"
+accountCity = "mum"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
