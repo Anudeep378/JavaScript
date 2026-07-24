9b5865c998c6970425dc20c3a376e528739a638b
Anudeep Singh <anudeepsinghh@gmail.com>
2026-07-24 14:53:52 +0000

    maths and num

---
+++ 01_basics/nums_math.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score);
+
+const val = new Number(500.9776)
+// console.log(val);
+
+// console.log(val.toFixed(2))
+
+const ohterNum = 123.478
+
+// console.log(ohterNum.toPrecision(2))//return string
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+//====================== Maths ===================
+
+// console.log(Math.abs(-7))
+
+// console.log(Math.round(5.6))//ciel -> higher val , floor->lower value
+
+console.log(Math.random()); //0 - 1 ranges
+console.log((Math.random() * 10) + 1);
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+
