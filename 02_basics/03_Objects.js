d2d2c3f8228ba0b73d065edc3bd391ee5cd2dece
Anudeep Singh <anudeepsinghh@gmail.com>
2026-07-27 17:35:52 +0000

    continue

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -10,5 +10,27 @@ const JsUser = {
     email : "anudeep@gmail.com",
 
 }
+//"str" : val then access through jsUser["str"]
 
-console.log(JsUser[mySym])
+// console.log(JsUser[mySym])
+
+JsUser.email = "xyz@gmail.com"
+//Object.freeze(JsUser)
+JsUser.email = "abc@gmail.com"
+// console.log(JsUser.email)
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello user");
+}
+// console.log(JsUser.greeting());
+
+JsUser.greeting2 = function(){
+    console.log(`Hello user, ${this.name} !`);
+}
+console.log(JsUser.greeting2());
+
+
+
+
+
