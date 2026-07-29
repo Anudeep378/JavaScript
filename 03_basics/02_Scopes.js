c4485d418298481435d5f6faaacbe4f86e264f69
Anudeep Singh <anudeepsinghh@gmail.com>
2026-07-29 09:09:27 +0000

    scopes

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -3,17 +3,43 @@
 // var c = 30
 
 
+let a = 300 // globalscope
 //{} // scope
 if(true){
-    let a = 10
+    let a = 10 
     const b = 20
-    var c = 30
+    console.log(a);    
 }
 
-// console.log(a)
+ console.log(a)
 // console.log(b);
 
 //
-console.log(c);// prints 30 but not 10 and 20
+// console.log(c);// prints 30 but not 10 and 20
+function one(){
+    const username = "anudeep"
+
+    function two(){ // for two, one is like global scope so it can access
+        const age = 19
+        console.log(username);
+        
+    }
+    //console.log(age); // one cant access two 
+     two()
+}
+one()
+//---------------------------------
+console.log(add1(9));// executes
+
+function add1(num){
+    return num+1
+}
+
+// console.log(value(6)); gives error
+
+let value = function(num){
+    return num+3
+}
+
 
 
